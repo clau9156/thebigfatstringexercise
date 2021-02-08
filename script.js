c590e4748e0fc606a7b15f81c729935cff8389ef
clau9156 <clau9156@stud.kea.dk>
2021-02-08 20:06:19 +0100

    javascript if else for each value (not done)

---
+++ script.js
@@ -0,0 +1,66 @@
+if (operator === "1") {
+
+    } else if (operator === "2") {
+        const firstName = "";
+        const firstLetter = firstName.substring(0,1).toUpperCase();
+        // 1- what? slice??
+        const rest = firstName.substring(1,).toLowerCase();
+        console.log(firstLetter+rest);
+
+    } else if (operator === "3") {
+        const fullName = " ";
+        const firstSpace = fullName.indexOf(" ");
+        // const lastSpace = fullName.lastIndexOf(" ");
+
+        const firstName = fullName.substring(0,firstSpace);
+        console.log(firstName);
+
+    } else if (operator === "3") {
+        const fullName = " ".split(' ');
+        const firstName = fullName[0];
+        const firstNameLength = firstName.length;
+        console.log(firstNameLength);
+
+    } else if (operator === "4") {
+        const fullName = " ";
+        const firstSpace = fullName.indexOf(" ");
+        const lastSpace = fullName.lastIndexOf(" ");
+
+        const middleName = fullName.substring(firstSpace,lastSpace);
+        console.log(middleName);
+
+
+    } else if (operator === "5") {
+        const fileName = " ";
+        const png = fileName.endsWith(".png")
+        const jpg = fileName.endsWith(".jpg")
+        // not done
+        if (fileName) {
+            
+        } else if (fileName) {
+            
+        }
+
+    } else if (operator === "6") {
+        const password = " ";
+        const hide = password.replaceAll(" ","*");
+        // not done
+
+
+
+    } else if (operator === "7") {
+        const name = " ";
+        const firstPart = " ";
+        const thirdLetter = name.substring(2,3).toUpperCase();
+        // 1- what? slice??
+        const rest = firstName.substring(3,).toLowerCase();
+        console.log(firstPart+thirdLetter+rest);
+
+    } else if (operator === "8") {
+        const input = " ";
+        const hyphen = input.includes("-");
+        const space = input.includes(" ");
+
+    
+    }      
+
